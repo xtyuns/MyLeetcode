c5c4f941afaeba047fddc92c27aa5c9a8447868b
xtyuns <xtyuns@163.com>
2021-06-14 09:46:04 +0800

    [剑指 Offer 13] 机器人的运动范围

--- src/leetcode/editor/cn/JiQiRenDeYunDongFanWeiLcof.java
+++ src/leetcode/editor/cn/JiQiRenDeYunDongFanWeiLcof.java
@@ -4,24 +4,35 @@ package leetcode.editor.cn;
 
 public class JiQiRenDeYunDongFanWeiLcof {
     // copy method to here
+    // 已访问坐标的标记数组
+    boolean[][] visited = null;
+    // 可到达的坐标数量
+    int count = 0;
+
     public int movingCount(int m, int n, int k) {
-        boolean[][] mark = new boolean[m][n];
-        return dfs(m, n, k, 0, 0, 0, mark);
+        this.visited = new boolean[m][n];
+        // dfs 从左上角开始, 搜索所有坐标
+        dfs(m, n, k, 0, 0);
+        return this.count;
     }
 
-    private int dfs(int m, int n, int k, int x, int y, int count, boolean[][] mark) {
+    private void dfs(int m, int n, int k, int x, int y) {
         int sum = getBitSum(x) + getBitSum(y);
-        if (sum > k || x >= m || y >= n) return count;
-        if (!mark[x][y]) {
-            mark[x][y] = true;
-            count++;
-        }
-        count = dfs(m, n, k, x, y + 1, count, mark) + dfs(m, n, k, x + 1, y, count, mark);
-
-
-        return count;
+        // 不满足题意、坐标越界、已访问坐标, 都不再向下遍历
+        if (sum > k || x >= m || y >= n || visited[x][y]) return;
+        // 当前坐标符合题意
+        this.count++;
+        // 记录为已访问坐标
+        visited[x][y] = true;
+        // 向右搜索
+        dfs(m, n, k, x, y + 1);
+        // 向下搜索
+        dfs(m, n, k, x + 1, y);
     }
 
+    /**
+     * 计算某数字的各数位之和
+     */
     private int getBitSum(int num) {
         int sum = 0;
         while (num > 0) {
@@ -32,7 +43,7 @@ public class JiQiRenDeYunDongFanWeiLcof {
     }
 
     public void debug() {
-
+        System.out.println(movingCount(36, 11, 15));
     }
 
     public static void main(String[] args) {
--- src/leetcode/editor/cn/readme.md
+++ src/leetcode/editor/cn/readme.md
@@ -3,7 +3,9 @@
 ## 03. 数组中重复的数字
 
 > 一、ArrayList.contains() 方法遍历nums，超时
+>
 > 二、HashSet 返回添加失败的元素，ok
+>
 > 三、原地索引交换，perfect
 
 ```java
@@ -344,6 +346,56 @@ class Solution {
         board[x][y] = word.charAt(k);
 
         return flag;
+    }
+}
+```
+
+
+
+
+
+## 13. 机器人的运动范围
+
+> dfs + 剪枝, ok
+
+```java
+class Solution {
+    // 已访问坐标的标记数组
+    boolean[][] visited = null;
+    // 可到达的坐标数量
+    int count = 0;
+
+    public int movingCount(int m, int n, int k) {
+        this.visited = new boolean[m][n];
+        // dfs 从左上角开始, 搜索所有坐标
+        dfs(m, n, k, 0, 0);
+        return this.count;
+    }
+
+    private void dfs(int m, int n, int k, int x, int y) {
+        int sum = getBitSum(x) + getBitSum(y);
+        // 不满足题意、坐标越界、已访问坐标, 都不再向下遍历
+        if (sum > k || x >= m || y >= n || visited[x][y]) return;
+        // 当前坐标符合题意
+        this.count++;
+        // 记录为已访问坐标
+        visited[x][y] = true;
+        // 向右搜索
+        dfs(m, n, k, x, y + 1);
+        // 向下搜索
+        dfs(m, n, k, x + 1, y);
+    }
+
+    /**
+     * 计算某数字的各数位之和
+     */
+    private int getBitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
 }
 ```
