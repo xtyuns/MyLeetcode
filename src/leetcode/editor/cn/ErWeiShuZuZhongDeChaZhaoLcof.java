6041a0ae0b7b3450db15ac42a116bbb00739a05e
xtyuns <xtyuns@163.com>
2022-01-04 19:36:39 +0800

    【二刷】[剑指 Offer 04] 二维数组中的查找

--- src/leetcode/editor/cn/ErWeiShuZuZhongDeChaZhaoLcof.java
+++ src/leetcode/editor/cn/ErWeiShuZuZhongDeChaZhaoLcof.java
@@ -1,38 +1,35 @@
 package leetcode.editor.cn;
 // [剑指 Offer 04] 二维数组中的查找
-// 2021-06-12 10:20:55
+// 2022-01-04 19:07:54
 
 public class ErWeiShuZuZhongDeChaZhaoLcof {
-    // copy method to here
-    public boolean findNumberIn2DArray(int[][] matrix, int target) {
-        boolean flag = false;
-        int rows = matrix.length;
-        if (rows == 0) {
+  String id = "剑指 Offer 04";
+  
+  public static void main(String[] args) {
+      Solution solution = new ErWeiShuZuZhongDeChaZhaoLcof().new Solution();
+  }
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean findNumberIn2DArray(int[][] matrix, int target) {
+            if (null == matrix || matrix.length == 0 || null == matrix[0] || matrix[0].length == 0)
+                return false;
+            int row = matrix.length;
+            int col = matrix[0].length;
+
+            // 从右上角可以将二维数组看作是一个二叉搜索树
+            int x = 0;
+            int y = col - 1;
+            while (x < row && y >= 0) {
+                int cur = matrix[x][y];
+                if (cur == target) return true;
+                if (cur > target) y--;
+                else x++;
+            }
+
             return false;
         }
-        int cols = matrix[0].length;
-        int row = 0;
-        int col = cols - 1;
-        while (row < rows && col >= 0) {
-            if (matrix[row][col] > target) {
-                col--;
-            } else if (matrix[row][col] < target) {
-                row++;
-            } else {
-                flag = true;
-                break;
-            }
-        }
-
-        return flag;
     }
-    
-    public void debug() {
+    //leetcode submit region end(Prohibit modification and deletion)
 
-    }
-
-    public static void main(String[] args) {
-        ErWeiShuZuZhongDeChaZhaoLcof solution = new ErWeiShuZuZhongDeChaZhaoLcof();
-        solution.debug();
-    }
 }
