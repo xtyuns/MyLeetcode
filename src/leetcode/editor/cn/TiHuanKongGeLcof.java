4472c4e4b0c8f1409b6c3075df3c0a848c78695f
xtyuns <xtyuns@163.com>
2022-01-04 19:55:35 +0800

    【二刷】[剑指 Offer 05] 替换空格

--- src/leetcode/editor/cn/TiHuanKongGeLcof.java
+++ src/leetcode/editor/cn/TiHuanKongGeLcof.java
@@ -1,26 +1,30 @@
 package leetcode.editor.cn;
 // [剑指 Offer 05] 替换空格
-// 2021-06-12 16:56:06
+// 2022-01-04 19:41:49
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class TiHuanKongGeLcof {
-    // copy method to here
+  String id = "剑指 Offer 05";
+  
+  public static void main(String[] args) {
+      Solution solution = new TiHuanKongGeLcof().new Solution();
+      System.out.println(solution.replaceSpace("    "));
+  }
+  
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    /**
+     * s.replace(" ", "%20") 效率更高
+     */
     public String replaceSpace(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (c == ' ') {
-                sb.append("%20");
-            } else {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
+        String[] strings = s.split("");
+        return Arrays.stream(strings)
+                .map(str -> " ".equals(str) ? "%20" : str)
+                .collect(Collectors.joining());
     }
-    
-    public void debug() {
-    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
 
-    public static void main(String[] args) {
-        TiHuanKongGeLcof solution = new TiHuanKongGeLcof();
-        solution.debug();
-    }
 }
