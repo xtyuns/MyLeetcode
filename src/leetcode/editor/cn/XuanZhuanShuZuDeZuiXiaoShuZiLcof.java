3df7bb7bc75bb43f82847c53c138f94cd4d9ab5e
xtyuns <xtyuns@163.com>
2022-01-07 20:27:09 +0800

    【二刷】[剑指 Offer 11] 旋转数组的最小数字

--- src/leetcode/editor/cn/XuanZhuanShuZuDeZuiXiaoShuZiLcof.java
+++ src/leetcode/editor/cn/XuanZhuanShuZuDeZuiXiaoShuZiLcof.java
@@ -1,32 +1,26 @@
 package leetcode.editor.cn;
 // [剑指 Offer 11] 旋转数组的最小数字
-// 2021-06-13 15:00:17
+// 2022-01-07 20:12:20
 
 public class XuanZhuanShuZuDeZuiXiaoShuZiLcof {
-    // copy method to here
+  String id = "剑指 Offer 11";
+  
+  public static void main(String[] args) {
+      Solution solution = new XuanZhuanShuZuDeZuiXiaoShuZiLcof().new Solution();
+  }
+  
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
     public int minArray(int[] numbers) {
-        int low = 0, high = numbers.length - 1;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (numbers[mid] > numbers[high]) {  // mid 大于 high, 说明最小值在右侧且 mid 位置必定不可能为最小值
-                low = mid + 1;
-            } else if (numbers[mid] < numbers[high]) {  // mid 小于 high, 说明最小值一定在左侧(数组递增)
-                high = mid;
-            } else {  // mid 等于 high, 无法确定最小值的位置, 进一步缩小问题 ([2,2,2,1,2] 和 [1,2,2,2,2])
-                high--;
+        // 因为数组是从递增进行的反转, 因此找到水位下落的转折点即为反转前的起点
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < numbers[i - 1]) {
+                return numbers[i];
             }
         }
-
-        return numbers[low];
+        return numbers[0];
     }
+}
+//leetcode submit region end(Prohibit modification and deletion)
 
-    public void debug() {
-        int[] s = new int[]{2,2,2,0,1};
-        minArray(s);
-    }
-
-    public static void main(String[] args) {
-        XuanZhuanShuZuDeZuiXiaoShuZiLcof solution = new XuanZhuanShuZuDeZuiXiaoShuZiLcof();
-        solution.debug();
-    }
 }
